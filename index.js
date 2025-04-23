74f83c85e867336dd2412273901f26320ab23629
patty-yang <643017440@qq.com>
2025-04-23 12:36:41 +0800

    feat: express v1

--- index.js
+++ index.js
@@ -12,3 +12,16 @@
 
 // const result = adminService.login('1', '123123')
 // console.log(result)
+
+const express = require('express')
+
+const app = express()
+
+app.get('/api/admin/login', (req, res) => {
+  const { loginId, loginPwd } = req.query
+  console.log(loginId, loginPwd)
+})
+
+app.listen(9527, () => {
+  console.log('server is running at http://localhost:9527')
+})
