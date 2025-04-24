7dc7fb973a5ee6f3e7b03dcb0a2b39f8c31d3459
patty-yang <643017440@qq.com>
2025-04-24 12:38:52 +0800

    feat: 使用官方的中间件

--- api/init.js
+++ api/init.js
@@ -1,19 +1,29 @@
 const express = require('express')
-
+const AdminService = require('../services/admin')
 const app = express()
 
-app.get('/api/test', (req, res, next) => {
-  next(new Error('error'))
-  // next()
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// app.get('/api/test', (req, res, next) => {
+//   next(new Error('error'))
+//   // next()
+// })
 
-app.get('/api/test', (req, res, next) => {
+// app.get('/api/test', (req, res, next) => {
+//   res.send({
+//     code: 200,
+//     msg: 'success'
+//   })
+// })
+
+app.post('/api/admin', async (req, res, next) => {
+  await AdminService.createAdmin(req.body)
+
   res.send({
     code: 200,
     msg: 'success'
   })
 })
-
 app.use(require('./errorMiddleware'))
 
 app.listen(9527, () => {
